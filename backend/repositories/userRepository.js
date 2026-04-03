b3a3b9c6e02fb95c74626fa4150ef3718957723c
Catrina Lim <catrina.lim8@gmail.com>
2026-04-03 17:02:57 -0400

    edited userRepository to account for db change

--- backend/repositories/userRepository.js
+++ backend/repositories/userRepository.js
@@ -1,15 +1,15 @@
 const db = require('../db');
 
-const findAll = () => db.query('SELECT * FROM users')
+const findById = (id) =>
+  db.query('SELECT id, username, email, created_at FROM users WHERE id = $1', [id]);
 
-const findById = (id) => db.query('SELECT * FROM users WHERE id = $1', [id]);
+const findByEmail = (email) =>
+  db.query('SELECT * FROM users WHERE email = $1', [email]);
 
-const create = (name, email) =>
-    db.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
+const create = (username, email, password_hash) =>
+  db.query(
+    'INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING id, username, email',
+    [username, email, password_hash]
+  );
 
-const update = (id, name, email) =>
-    db. query('UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING *', [name, email, id]);
-
-const remove = (id) => db.query('DELETE FROM users WHERE id = $1', [id]);
-
-module.exports = { findAll, findById, create, update, remove };
+module.exports = { findById, findByEmail, create };
