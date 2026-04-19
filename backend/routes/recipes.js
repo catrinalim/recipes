2ddddd9e2dd0f38c838d85182ef5567bb157c1b6
Catrina Lim <catrina.lim8@gmail.com>
2026-04-19 00:20:49 -0400

    udpated recipes.js

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipeController');
 
+// GET all recipes for a user
 router.get('/', recipeController.getAllRecipes);
+
+// GET single recipe
 router.get('/:id', recipeController.getRecipeById);
+
+// POST create recipe
 router.post('/', recipeController.createRecipe);
+
+// PUT update recipe
 router.put('/:id', recipeController.updateRecipe);
+
+// DELETE recipe
 router.delete('/:id', recipeController.deleteRecipe);
+
+// POST get recipe suggestions based on inventory
 router.post('/suggestions', recipeController.getSuggestions);
 
 module.exports = router;
