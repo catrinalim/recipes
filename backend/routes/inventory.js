3502c3baf545e91cac3140e2577df07392f0e566
Catrina Lim <catrina.lim8@gmail.com>
2026-04-19 00:21:19 -0400

    added comments to inventory router

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 const inventoryController = require('../controllers/inventoryController');
 
-router.get('/', inventoryController.getUserInventory);
+// GET user inventory
+router.get('/', inventoryController.getInventory);
+
+// POST add ingredient to inventory
 router.post('/', inventoryController.addIngredient);
+
+// PUT update ingredient in inventory
 router.put('/:id', inventoryController.updateIngredient);
+
+// DELETE ingredient from inventory
 router.delete('/:id', inventoryController.deleteIngredient);
 
 module.exports = router;
