7c4426283ce594834596ed9f5370c7e5043e3d6d
Catrina Lim <catrina.lim8@gmail.com>
2026-04-19 00:11:34 -0400

    edited route name

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const inventoryController = require('../controllers/inventoryController');
 
-router.get('/', inventoryController.getInventory);
+router.get('/', inventoryController.getUserInventory);
 router.post('/', inventoryController.addIngredient);
 router.put('/:id', inventoryController.updateIngredient);
 router.delete('/:id', inventoryController.deleteIngredient);
