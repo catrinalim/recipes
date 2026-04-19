0bde53a44041e2d5c7c4212bd0b2ffc7a6c811e2
Catrina Lim <catrina.lim8@gmail.com>
2026-04-19 15:53:32 -0400

    removed template files and added stack screens

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,7 +4,11 @@ export default function Layout() {
   return (
     <Stack initialRouteName="index">
       <Stack.Screen name="index" options={{ title: 'Lettuce Eat' }}/>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="recipes/index" options={{ title: 'My Recipes' }} />
+      <Stack.Screen name="recipes/[id]" options={{ title: 'Recipe Details' }} />
+      <Stack.Screen name="recipes/add" options={{ title: 'Add Recipe' }} />
+      <Stack.Screen name="inventory" options={{ title: 'My Inventory' }}/>
+      <Stack.Screen name="suggestions" options={{ title: 'Recipe Suggestions' }}/>
     </Stack>
   );
 }
